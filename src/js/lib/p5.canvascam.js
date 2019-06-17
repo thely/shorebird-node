03ea52ded3eae615a31873f51d7f18997ddd9dff
thely <bardbarienne@gmail.com>
2019-06-16 22:40:18 -0400

    adapted this thing for node

--- src/js/lib/p5.canvascam.js
+++ src/js/lib/p5.canvascam.js
@@ -13,13 +13,18 @@
 
 import p5 from 'p5';
 
-p5.prototype.Camera = function(zoom, tx, ty) {
+p5.prototype.CanvasCam = function(zoom, tx, ty) {
 
   // variables for storing defaults
   var zoom0, tx0, ty0;
 
+  var limit = {};
+  var dim = {};
+
   // self reference
   var cam = this;
+  var w = this.height;
+  var h = this.width;
 
   // reset to defaults
   reset(zoom, tx, ty);
@@ -36,39 +41,41 @@ p5.prototype.Camera = function(zoom, tx, ty) {
   var _updateNextMouseCoords = p5.prototype._updateNextMouseCoords;
   p5.prototype._updateNextMouseCoords = function(e) {
     _updateNextMouseCoords.bind(this)(e);
-    cam.mouseX = camX(this.mouseX);
-    cam.mouseY = camY(this.mouseY);
-    // this._setProperty('camMouseX', cam.mouseX);
-    // this._setProperty('camMouseY', cam.mouseY);
+    cam.mouseX = camX.call(this, this.mouseX);
+    cam.mouseY = camY.call(this, this.mouseY);
   };
 
   // update previous mouse coordinates of the camera
   var _updateMouseCoords = p5.prototype._updateMouseCoords;
   p5.prototype._updateMouseCoords = function(e) {
     _updateMouseCoords.bind(this)(e);
-    cam.pmouseX = camX(this.pmouseX);
-    cam.pmouseY = camY(this.pmouseY);
-    // this._setProperty('pcamMouseX', cam.pmouseX);
-    // this._setProperty('pcamMouseY', cam.pmouseY);
+    cam.pmouseX = camX.call(this, this.pmouseX);
+    cam.pmouseY = camY.call(this, this.pmouseY);
   };
 
   // reset the matrix to camera defaults (called at the beginning of every redraw function)
   p5.prototype.resetMatrix = function() {
     this._renderer.resetMatrix();
-    p5.translate(p5.width/2, p5.height/2);
-    p5.scale(zoom);
-    p5.translate(-tx, -ty);
+    this.translate(this.width/2, this.height/2);
+    this.scale(zoom);
+    this.translate(-tx, -ty);
     return this;
   };
 
   // cam x-coord to canvas x-coord
   function camX(x) {
-    return tx + (x - p5.width/2) / zoom;
+    return tx + (x - this.width/2) / zoom;
   }
 
   // cam y-coord to canvas y-coord
   function camY(y) {
-    return ty + (y - p5.height/2) / zoom;
+    return ty + (y - this.height/2) / zoom;
+  }
+
+  function constrain(val, min, max) {
+    let x = (val <= min) ? min : val;
+    x = (val >= max) ? max : x;
+    return x;
   }
 
   // expose reset function
@@ -77,17 +84,43 @@ p5.prototype.Camera = function(zoom, tx, ty) {
   // rescale the camera relative to the center
   this.scale = function(factor, centerX, centerY) {
     var newZoom = zoom * factor;
-    var dx = centerX - width/2;
-    var dy = centerY - height/2;
+    var dx = centerX - dim.x/2;
+    var dy = centerY - dim.y/2;
     tx += dx/zoom - dx/newZoom;
     ty += dy/zoom - dy/newZoom;
     zoom = newZoom;
+
+    return zoom;
   };
 
   // translate the origin of the camera's coordinate system
   this.translate = function(dx, dy) {
-    tx += dx;
-    ty += dy;
+    tx = constrain(tx + dx, limit.x.min, limit.x.max);
+    ty = constrain(ty + dy, limit.y.min, limit.y.max);
   };
 
+  this.getPanning = function() {
+    return {x: tx, y: ty};
+  }
+
+  this.dimensions = function(view, map) {
+    dim.x = view.x;
+    dim.y = view.y;
+    limit.x = {
+      min: view.x / 2,
+      max: map.x - (view.x / 2)
+    };
+    limit.y = {
+      min: view.y / 2,
+      max: map.y - (view.y / 2)
+    };
+    console.log("dimensions: ");
+    console.log(limit);
+
+    return limit;
+  }
+
+
 };
+
+export default p5.prototype.CanvasCam;
